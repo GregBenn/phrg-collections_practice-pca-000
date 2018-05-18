832f650d48c22e42453f1346d3d6a9219bea974d
GregBenn <gbenn07@gmail.com>
2018-05-18 12:06:56 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,62 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
+
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+
+def kesha_maker(array)
+  array.each do |s|
+    s[2] = "$"
+  end
+end
+
+
+def find_a(array)
+  array.select do |s|
+   s.start_with?("a")
+  end
+end
+
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+
+def add_s(array)
+  array.collect do |a|
+    if array[1] == a
+    a
+    else
+    a + "s"
+    end
+  end
+end
